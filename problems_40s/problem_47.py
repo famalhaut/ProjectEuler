2348a0e9b432695b2bd57251a3c90a1b15058765
famalhaut <famalhaut.ru@gmail.com>
2017-04-19 14:30:40 +0300

    fix 47

--- problems_40s/problem_47.py
+++ problems_40s/problem_47.py
@@ -16,138 +16,57 @@ Find the first four consecutive integers to have four distinct prime factors eac
 What is the first of these numbers?
 """
 from common.primes import factorization, primes_gen
-from time import time
 import functools
 
-@functools.lru_cache(10)
-def foo(x, q=None):
-    amount = 0
-    if x == 1:
-        return False
-    for p in primes_gen():
-        if p * p > x:
-            if x == 1:
-                return amount == q
+prs_gen = primes_gen()
+primes = [next(prs_gen), ]
+
+
+def pretty_input(x):
+    fact = ' * '.join('{}^{}'.format(pr, pw) for pr, pw in sorted(factorization(x).items()))
+    return '{} = {}'.format(x, fact)
+
+
+@functools.lru_cache(maxsize=None)
+def factorization(x):
+    for i, prime in enumerate(primes, start=1):
+        if prime ** 2 > x:
+            return {x: 1}
+        if x % prime == 0:
+            x //= prime
+            result = factorization(x).copy()
+            if prime in result:
+                result[prime] += 1
             else:
-                return (amount + 1) == q
-        if x % p == 0:
-            amount += 1
-            if amount > q:
-                return False
-        while x % p == 0:
-            x //= p
-#######################################################################################
-
-# st = time()
-# i = 14
-# while True:
-#
-#     if len(factorization(i)) == 4:
-#         if len(factorization(i + 1)) == 4:
-#             if len(factorization(i + 2)) == 4:
-#                 if len(factorization(i + 3)) == 4:
-#                     print(sorted(factorization(i + 0).items()))
-#                     print(sorted(factorization(i + 1).items()))
-#                     print(sorted(factorization(i + 2).items()))
-#                     print(sorted(factorization(i + 3).items()))
-#                     print(i)
-#                     break
-#                 else:
-#                     i += 4
-#             else:
-#                 i += 3
-#         else:
-#             i += 2
-#     else:
-#         i += 1
-# print(time() - st)
+                result[prime] = 1
+            return result
+        if i == len(primes):
+            primes.append(next(prs_gen))
 
 
-############################################################################################
+def problem():
+    num = 3 * 5 * 7
+    while True:
+        if ((num % 2 == 0 and len(factorization(num)) == 4 and len(factorization(num + 1)) == 3) or
+                (num % 2 == 1 and len(factorization(num)) == 3 and len(
+                    factorization(num + 1)) == 4)):
+            if len(factorization(2 * num + 1)) == 4:
+                if len(factorization(2 * num - 1)) == 4:
+                    for i in range(2 * num - 1, 2 * num + 3):
+                        print(pretty_input(i))
+                    return 2 * num - 1
+                elif len(factorization(2 * num + 3)) == 4:
+                    for i in range(2 * num, 2 * num + 4):
+                        print(pretty_input(i))
+                    return 2 * num
+                else:
+                    num += 2
 
-@functools.lru_cache(10)
-def factorization2(x):
-    return factorization(x)
-
-
-st = time()
-i = 3 * 5 * 7
-while True:
-
-    fact_i0 = factorization2(i)
-    fact_i1 = factorization2(i + 1)
-
-    if (i % 2 == 0 and len(fact_i0) == 4 and len(fact_i1) == 3) or \
-            (i % 2 == 1 and len(fact_i0) == 3 and len(fact_i1) == 4):
-        if len(factorization2(2 * i + 1)) == 4:
-            if len(factorization2(2 * i - 1)) == 4:
-                print('-1')
-
-                print(fact_i0, fact_i1)
-
-                print(sorted(factorization2(2 * i - 1).items()))
-                print(sorted(factorization2(2 * i + 0).items()))
-                print(sorted(factorization2(2 * i + 1).items()))
-                print(sorted(factorization2(2 * i + 2).items()))
-
-                print(2 * i - 1)
-                break
-            elif len(factorization2(2 * i + 3)) == 4:
-                print('+3')
-                print(fact_i0, fact_i1)
-                print(sorted(factorization2(2 * i + 0).items()))
-                print(sorted(factorization2(2 * i + 1).items()))
-                print(sorted(factorization2(2 * i + 2).items()))
-                print(sorted(factorization2(2 * i + 3).items()))
-
-                print(2 * i)
-                break
             else:
-                i += 2
-
+                num += 1
         else:
-            i += 1
-    else:
-        i += 1
-
-print(time() - st)
+            num += 1
 
 
-##################################################################################################
-
-st = time()
-i = 3 * 5 * 7
-while True:
-
-    if (i % 2 == 0 and foo(i, 4) and foo(i + 1, 3)) or \
-            (i % 2 == 1 and foo(i, 3) and foo(i + 1, 4)):
-        if foo(2 * i + 1, 4):
-            if foo(2 * i - 1, 4):
-                print('-1')
-
-                print(sorted(factorization2(2 * i - 1).items()))
-                print(sorted(factorization2(2 * i + 0).items()))
-                print(sorted(factorization2(2 * i + 1).items()))
-                print(sorted(factorization2(2 * i + 2).items()))
-
-                print(2 * i - 1)
-                break
-            elif foo(2 * i + 3, 4):
-                print('+3')
-
-                print(sorted(factorization2(2 * i + 0).items()))
-                print(sorted(factorization2(2 * i + 1).items()))
-                print(sorted(factorization2(2 * i + 2).items()))
-                print(sorted(factorization2(2 * i + 3).items()))
-
-                print(2 * i)
-                break
-            else:
-                i += 2
-
-        else:
-            i += 1
-    else:
-        i += 1
-
-print(time() - st)
+if __name__ == '__main__':
+    print('Answer:', problem())
