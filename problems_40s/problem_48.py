dc3ee12f7de0e09b0f0a3348f638a458fcc95aa3
famalhaut <famalhaut.ru@gmail.com>
2017-04-19 14:39:39 +0300

    problem 48

---
+++ problems_40s/problem_48.py
@@ -0,0 +1,20 @@
+"""
+Self powers
+Problem 48 
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+"""
+
+
+def problem():
+    result = 0
+    mod = 10 ** 10
+    for num in range(1, 1001):
+        result += pow(num, num, mod)
+
+    return result % mod
+
+
+if __name__ == '__main__':
+    print('Answer:', problem())
